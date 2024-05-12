1cdf4406a575e8c20e25e7eed333e80b31906923
Juliano Marangoni <julianomlp@hotmail.com>
2024-05-12 00:02:25 -0300

    atualizando conteudo

---
+++ TiposVariaveis/src/TipoDados.java
@@ -0,0 +1,12 @@
+public class TipoDados {
+    public static void main(String[]args){
+        byte idade = 123;
+        short ano = 2024;
+        int cep = 16054160; //se começar com 0 tem que ser outro tipo
+        long cpf = 09817241912L; //L no final do long, cpf = 09817241912, se iniciar com 0 tem que ser outro tipo
+        float pi = 3.14F; //sempre indicar o F no final do tipo float
+        double slario = 2500.33;
+
+    }
+
+}
---
+++ TiposVariaveis/src/tiposvariaveis.java
@@ -0,0 +1,12 @@
+public class tiposvariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int nummeroNormal = numeroCurto;
+        short numeroCurto2 = numeroNormal;
+
+        final double NUMERO_IMUTAVEL = 3.14; //declaração de constante
+
+    }
+}
